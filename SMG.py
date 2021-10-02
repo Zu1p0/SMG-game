ea68abc1ed3a95af62ac7ddce52bca728b1510b2
Zu1p0 <66071965+Zu1p0@users.noreply.github.com>
2021-10-02 18:50:57 +0200

    bugs fix, add color

--- SMG.py
+++ SMG.py
@@ -4,21 +4,27 @@ Created on Fri Jun 18 09:43:54 2021
 
 @author: anton
 
-todo:
-    stop program when fall down a lot (looking at maze from below)
-    use health bar to set max time to find exit (-1 per second)
 """
 
 from ursina import *
-from time import time
+import ursina.main as main
+import maze_generator 
+from ursina.prefabs.first_person_controller import FirstPersonController
+from ursina.prefabs.health_bar import HealthBar
+from time import time, sleep
 
 
 app = Ursina()
-window.fullscreen = True
-
-playing = False
+window.fullscreen = False
 
 
+class Data:
+    player = None
+    level = 10
+    blocklist = []
+    opti = True
+    label = None
+    
 class Block(Button):
     
     def __init__(self, position=(0,0,0), coloring = color.color(0, 0, random.uniform(.9, 1.0))):
@@ -34,14 +40,32 @@ class Block(Button):
         
     def input(self, key):
         pass
-  
+
+
+def create_square(i,y,j, opti = False, color=color.white):
+    global data
+    if data.opti == False:
+        i = i*2
+        j = j*2
+        data.blocklist.append(Block(position = (i,y,j), coloring=color))
+        data.blocklist.append(Block(position = (i+1,y,j), coloring=color))
+        data.blocklist.append(Block(position = (i,y,j+1), coloring=color))
+        data.blocklist.append(Block(position = (i+1,y,j+1), coloring=color))
+    else :
+        data.blocklist.append(Block(position = (i,y,j), coloring=color))
 
 def start_game():
-    import maze_generator as gen
-    from ursina.prefabs.first_person_controller import FirstPersonController
-    from ursina.prefabs.health_bar import HealthBar
-    window.color = color.dark_gray 
+    global data
+    window.color = color.dark_gray
+
+    if data.player != None:
+        data.player.ignore = True
+        data.player.ignore_input = True
+
+    gen = maze_generator.Maze(height = round(data.level), width = round(data.level))
     
+    for block in data.blocklist : destroy(block)
+
     for i in range(0, gen.height):
         for j in range(0, gen.width):
             
@@ -49,26 +73,36 @@ def start_game():
                 raise Exception("Generator problem")
                 
             elif (gen.maze[i][j] == 'c'):
-                Block(position = (i,0,j))
+                create_square(i,0, j, opti = data.opti)
             
             elif gen.maze[i][j] == 's' :
-                Block(position = (i,0,j))
-                startpoint = (i,0,j)
+                create_square(i,0, j, opti = data.opti, color = color.green)
+                if data.opti == False : startpoint = (i*2,0,j*2)
+                else : startpoint = (i,0,j)
                 
             elif gen.maze[i][j] == 'e' :
-                Block(position = (i,0,j))
-            
+                create_square(i,0, j, opti = data.opti, color = color.blue)
+                if data.opti == False : endpoint = (i*2,0,j*2)
+                else : endpoint = (i,0,j)
+                
             else:
                 for y in range(4):
-                    Block(position = (i,y,j))
+                    create_square(i,y, j, opti = data.opti)
 
+    if data.player != None :
+        data.player.reboot(startpoint=startpoint, endpoint = endpoint)
+    else:
+        data.player = Player(startpoint=startpoint, endpoint = endpoint)
+        data.player.mouse_sensitivity = (30,30)
+    sky = Sky()
+    data.player.ignore = False
+    data.player.ignore_input = False
 
-    player = FirstPersonController(z = startpoint[2])
 
 def start_screen():
     window.color = color.dark_gray
     title = Entity(parent = camera.ui,
-            position = (0,0.2,0),
+            position = (0,0.14,0),
             model = "quad",
             texture = "logo.png")
     title.scale = 1.2
@@ -76,35 +110,77 @@ def start_screen():
     
     class KeyClick(Button):
         def __init__(self):
-            super().__init__(parent = camera.ui, position = (0,-0.2,0), text = "", icon = "keytostart.png")
-            self.scale = 0.2
-            self.scale_y = 0.1
-            self.scale_x = 0.9
+            super().__init__(parent = camera.ui, position = (0,-0.2,0), text = "", icon = "start.png")
+            self.scale = 0.3
+            self.scale_y = 0.14
+            self.scale_x = 1
             self.color = color.rgb(255,255,255, a=0)
             self.highlight_color = self.color
             self.pressed_color = self.color
             self.it = 0
-        def on_click(self): 
-            start_game()
-            destroy(self)
-            destroy(title)
-            playing = True
         def update(self):
-            self.it += 1
-            if self.it == 20:
-                if self.visible == False: self.visible = True
-                else : self.visible = False
-                self.it = 0
+            if held_keys["enter"] :
+                start_game()
+                destroy(self)
+                destroy(title)
+                data.label = LevelLabel()
+            
+            self.it += 2 * main.time.dt
+            if self.visible == False: 
+                if self.it > 1 : 
+                    self.visible = True
+                    self.it = 0
+            else : 
+                if self.it > 2 : 
+                    self.visible = False
+                    self.it = 0
+            
+            print(self.it)
         
     click = KeyClick()
     
 
+class Player(FirstPersonController):
+    def __init__(self, startpoint, endpoint):
+        super().__init__(z = startpoint[2])
+        self.startpoint = startpoint
+        self.endpoint = endpoint
+    def restart(self):
+        self.x = self.startpoint[0]
+        self.z = self.startpoint[2]
+    def reboot(self, startpoint, endpoint):
+        self.startpoint = startpoint
+        self.endpoint = endpoint
+        self.x = self.startpoint[0]
+        self.z = self.startpoint[2]
+
+class LevelLabel(Text):
+    def __init__(self, **kwargs):
+        super().__init__(master = camera.ui, **kwargs)
+        self.size = 0.03
+        self.color = color.black
+        self.x = .5
+        self.y = .4
+
+    def update(self):
+        global data
+        self.text = "level: "+str(data.level)
+        
 def update():
-    pass
+    global player, level
+    if data.player != None:
+        data.label.update()
+        if held_keys["r"]: data.player.restart()
+        if round(data.player.x) == data.player.endpoint[0]: #and round(data.player.z) == data.player.endpoint[2]:
+            if data.level <30 : data.level += 2
+            if data.level == 30 : data.opti = True
+            start_game()
+    if held_keys["q"]: quit()
 
 msc = Audio("backgroundmusic.mp3", loop = True)
 msc.play()
 
 start_screen()
+data = Data()
 
 app.run()
